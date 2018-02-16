0c808b24311783cc2a519d6e1f937f682b2e4699
Mireille Bobbert <mlfbobbert@gmail.com>
2018-02-16 10:01:35 +0100

    Added v0.1 of form.php and adapted scss due to bootstrap errors when running gulp build

---
+++ src/form.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputId">ID</label>
+      <input type="text" name="id" class="form-control" id="inputId" placeholder="ID">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputName">Name</label>
+      <input type="text" name="name" class="form-control" id="inputName" placeholder="First and last name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputEmail">Email</label>
+    <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Seat number</label>
+    <input type="text" name="seatNumber" class="form-control" id="inputSeatNumber" placeholder="Seat Number">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
--- src/index.php
+++ src/index.php
@@ -1,2 +1,3 @@
 // This is gonna be the awesome cinema booking made by Mireille Eva and Tam
 //(eventhough it's a bit suspicious what the catch is. :D )
+
