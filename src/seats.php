fec1b74c270bb7b56fd137b5f7c08ea2e0ad42aa
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-02-16 13:00:56 +0100

    Prototype for the improvement

---
+++ src/seats.php
@@ -0,0 +1,32 @@
+
+<html>
+    <head>
+
+
+    </head>
+ <body>
+        <div id="seat-area">
+
+        <img src="img/seat.png" alt="">
+        <div class="occupied"></div>
+        <img src="img/seat.png" alt="">
+        <div class="available"></div>
+        <img src="img/seat.png" alt="">
+        <div class="occupied"></div>
+        <img src="img/seat.png" alt="">
+        <div class="available"></div>
+        <img src="img/seat.png" alt="">
+        <div class="available"></div>
+        <img src="img/seat.png" alt="">
+        <div class="available"></div>
+        <img src="img/seat.png" alt="">
+        <div class="occupied"></div>
+        <img src="img/seat.png" alt="">
+        <div class="available"></div>
+
+
+
+
+        </div>
+ </body>
+</html>
